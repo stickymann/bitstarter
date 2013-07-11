53d96d9889c4fe86a9b8e6f6f187cc393ddeab48
Dunstan Nesbit <dunstan.nesbit@gmail.com>
2013-07-10 23:25:26 -0400

    Added fs.writeFile function

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var infile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var text = fs.readFileSync(infile);
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
